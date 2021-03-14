584ade93bc93208decd97372e5d4ee40c6e83d0d
DESKTOP-IJ9JA1U\Jakob <jajajakob@freenet.de>
2021-03-15 00:33:40 +0100

    commit...

---
+++ Assets/SandroUI/UI/MenuScripts/GameOverMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public void Restart()
+    {
+        SceneManager.LoadScene("CheeseNuggets69");
+        Debug.Log("Restart Game");
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("StartMenu");
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+
+}
---
+++ Assets/SandroUI/UI/MenuScripts/PauseMenu.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUi;
+    [SerializeField] private GameObject BackGround;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && !GameIsPaused)
+        {
+                Pause();
+        }
+
+    }
+
+    public void Resume()
+    {
+        pauseMenuUi.SetActive(false);
+        BackGround.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+
+    void Pause ()
+    {
+        pauseMenuUi.SetActive(true);
+        BackGround.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+   
+ 
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 3f;
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting Game...");
+        Application.Quit();
+    }
+
+}
